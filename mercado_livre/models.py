27571b482a8689545d07a4ed26439cf2f976e3b3
lucasboscatti <lucasboscatti@gmail.com>
2022-04-07 16:12:35 -0300

    datetime without timezone

--- mercado_livre/models.py
+++ mercado_livre/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, ARRAY, Boolean, String, create_engine, DateTime, sql 
+from sqlalchemy import Column, ARRAY, Boolean, String, create_engine, DateTime
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 
 import os
 from decouple import AutoConfig
+import datetime
 
 CONFIG_DIR = os.path.join(os.path.dirname(__file__))
 config = AutoConfig(search_path=CONFIG_DIR)
@@ -41,4 +42,4 @@ class Items(DeclarativeBase):
     product_discount = Column("discount", String)
     product_old_price = Column("old_price", String)
     product_new_price =  Column("new_price", String)
-    date = Column(DateTime(timezone=True), default=sql.func.now())
+    date = Column(DateTime(timezone=False), default=datetime.datetime.now().strftime('%m/%d/%Y'))
