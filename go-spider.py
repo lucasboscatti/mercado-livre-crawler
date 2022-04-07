6b4ceef2034aa439008bca1542bea1bc297878fe
lucasboscatti <lucasboscatti@gmail.com>
2022-04-07 15:17:38 -0300

    run crawl

---
+++ go-spider.py
@@ -0,0 +1,8 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from mercado_livre.spiders.offers import OffersSpider
+
+if __name__=='__main__':
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(OffersSpider)
+    process.start()
