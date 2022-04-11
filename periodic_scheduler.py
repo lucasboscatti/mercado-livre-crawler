934e7a353bd0906a0de385d1962011259796ed61
lucasboscatti <lucasboscatti@gmail.com>
2022-04-11 14:28:34 -0300

    cron at 15:00

--- periodic_scheduler.py
+++ periodic_scheduler.py
@@ -15,8 +15,8 @@ def send_request():
 if __name__ == "__main__":
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     scheduler = TwistedScheduler(timezone=pytz.timezone("America/Sao_Paulo"))
-    # cron trigger that schedules job every every miday at 12:00
-    scheduler.add_job(send_request, "cron", day_of_week="*", hour=13, minute=35)
+    # cron trigger that schedules job every every day at 15:00
+    scheduler.add_job(send_request, "cron", day_of_week="*", hour=15, minute=0)
     # start the scheduler
     scheduler.start()
     reactor.run()
