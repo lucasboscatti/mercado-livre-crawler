29c5be09ff610f9487b42f4aeee4ee89dd8f794f
lucasboscatti <lucasboscatti@gmail.com>
2022-04-12 15:54:32 -0300

    requirements

--- periodic_scheduler.py
+++ periodic_scheduler.py
@@ -15,7 +15,7 @@ def send_request():
 if __name__ == "__main__":
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     scheduler = TwistedScheduler(timezone=pytz.timezone("America/Sao_Paulo"))
-    # cron trigger that schedules job every every miday at 12:00
+    # cron trigger that schedules job every every 30min
     scheduler.add_job(send_request, "cron", day_of_week="*", minute="*/30")
     # start the scheduler
     scheduler.start()
